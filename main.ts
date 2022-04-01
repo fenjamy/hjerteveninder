cd094b2bebd773690041c0184e040bf45a90b0d0
fenjamy <102808505+fenjamy@users.noreply.github.com>
2022-04-01 15:05:00 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,57 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . . # #
+        # # . . #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . . . . #
+        # . . . .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        # # . . .
+        . # # . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . # .
+        . . . # #
+        . . # # #
+        . . . # .
+        . . # . .
+        `)
+})
